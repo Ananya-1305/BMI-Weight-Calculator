71cc698de1532f4d05a05ccb22683ce040d0588a
Ananya-1305 <ananyapriya1305@gmail.com>
2025-07-14 20:26:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,21 @@ form.addEventListener('submit', function(e) {
 
    const height = parseInt(document.querySelector('#height').value)
    const weight = parseInt(document.querySelector('#weight').value)
-   const result = document.querySelector('#result').value
+   const result = document.querySelector('#result');
 
-   if(height === '' || height <0|| isNaN(height)) {
-       result.innerHTML = 'Please enter valid values for height.'
+    if (height === '' || height < 0 || isNaN(height)) {
+        result.innerHTML = `Please enter valid values for height ${height}.`
+    }
+
+    else if (weight === '' || weight < 0 || isNaN(weight)) {
+        result.innerHTML = `Please enter valid values for weight ${weight}.`
+    }
+
+    else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2)
+        result.innerHTML = `<span>${bmi}</span>  `;
+    }
+  
        
-   }
+   
 });
